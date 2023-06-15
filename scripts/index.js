2610b4d24934d5a9806148908d82f968fc872106
Luis <lsalcedo110@gmail.com>
2023-06-15 02:47:09 -0400

    lol

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,9 @@ const porfolioButton = document.querySelector('.porfolio-btn');
 porfolioButton.addEventListener('click', ()=>{
     window.open('projects.html', '_self')
 })
+
+
+const deployedButton = document.querySelector('.deployed-btn');
+deployedButton.addEventListener('click', ()=>{
+    window.open('https://apps.apple.com/us/app/ai-companion-pro/id6449543101', '_self')
+})
